5276179e0f08e31703b04cc3af7f5401556654e5
Muhammadqodir <Muhammadqodir@DESKTOP-SGHRIR2>
2025-05-19 14:02:15 +0500

    Add field to user

--- src/Portfolio.Domain/Entities/User.cs
+++ src/Portfolio.Domain/Entities/User.cs
@@ -10,7 +10,12 @@ public class User : Auditable
     public string Email { get; set; }
     public string UserName { get; set; }
     public string PasswordHash { get; set; }
-
+    public string? Telegram { get; set; }
+    public string? Github { get; set; }
+    public DateTime? Birthday { get; set; }
+    public string? Address { get; set; }
+    public string? PhoneNumbe { get; set; }
+    public string? Info { get; set; }
     public UserRole Role { get; set; }
 
     public ICollection<Education> Educations { get; set; }
