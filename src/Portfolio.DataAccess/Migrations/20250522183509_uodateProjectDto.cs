19749650aedab353c2b72b7407cee05f7b8b7f55
Muhammadqodir <Muhammadqodir@DESKTOP-SGHRIR2>
2025-05-23 00:15:58 +0500

    Update ProjectUpdateDto

---
+++ src/Portfolio.DataAccess/Migrations/20250522183509_uodateProjectDto.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Portfolio.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class uodateProjectDto : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "URL",
+                table: "Projects",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "URL",
+                table: "Projects");
+        }
+    }
+}
